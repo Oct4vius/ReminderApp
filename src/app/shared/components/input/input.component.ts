b0312074a4be5139ada88b20a667a9a3f0053f41
Octavio Luperón <armando2445@outlook.com>
2024-01-20 17:20:53 -0400

    formatted

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,25 +1,29 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'shared-input',
   templateUrl: './input.component.html',
 })
 export class InputComponent {
-
   @Input()
   public placeholder?: string;
 
   @Output()
-  public saveInputValue: EventEmitter<string> = new EventEmitter()
+  public saveInputValue: EventEmitter<string> = new EventEmitter();
 
-  @ViewChild("reminderInput")
+  @ViewChild('reminderInput')
   public reminderInput!: ElementRef<HTMLInputElement>;
 
-  public saveInput = ( value: string ) => {
-    this.saveInputValue.emit(value)
+  public saveInput = (value: string) => {
+    this.saveInputValue.emit(value);
 
-    this.reminderInput.nativeElement.value = ''
-
-  }
-
+    this.reminderInput.nativeElement.value = '';
+  };
 }
